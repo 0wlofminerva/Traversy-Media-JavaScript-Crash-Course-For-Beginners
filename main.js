cf7ef620dc836f5314e0adf0a1417c81c86d552c
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2021-01-03 18:36:33 +0100

    for & while loop

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
-const todos = [
-	{
-		id: 1,
-		text: 'Take out trash',
-		isCompleted: true,
-	},
-	{
-		id: 2,
-		text: 'Meeting with boss',
-		isCompleted: true,
-	},
-	{
-		id: 3,
-		text: 'Dentist appt',
-		isCompleted: false,
-	},
-];
-
-const todoJSON = JSON.stringify(todos);
-console.log(todoJSON);
-
-//
-
-[
-	{ id: 1, text: 'Take out trash', isCompleted: true },
-	{ id: 2, text: 'Meeting with boss', isCompleted: true },
-	{ id: 3, text: 'Dentist appt', isCompleted: false },
-];
-
-//
+// While
+let i = 0;
+while (i < 10) {
+	console.log(`While Loop Number: ${i}`);
+	i++;
+}
--- readme.md
+++ readme.md
@@ -850,6 +850,35 @@ JSON
 
     //
 
+Loops
+
+    // For
+    for(let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+
+    //
+
+    for(let i = 0; i<= 10; i++) {
+        console.log(i);
+    }
+
+    //
+
+    for(let i = 0; i <= 10; i++) {
+        console.log(`For Loop Number: ${i}`);
+    }
+
+    // While
+    let i = 0;
+    while(i < 10) {
+        console.log(`While Loop Number: ${i}`);
+        i++;
+    }
+
+    
+
+
 
 
 
